53448c472a6ba181e4b1531c0b9fba717d868558
Zycer <1171881160@qq.com>
2021-11-27 18:07:43 +0800

    fix: 完善ev数据生成以及其他过程

--- code/random_data.py
+++ code/random_data.py
@@ -2,48 +2,49 @@ import random
 import numpy as np
 
 
-def func1(x):
-    return 6 * x
+class EV:
 
+    attribute = [
+        "soc",
+        "time",
+        "distance"
+    ]
 
-def func2(x):
-    return 5.8 * x + 1.2
+    def __init__(self, name, k, b, t_discount):
+        self.name = name
+        self.k = k
+        self.b = b
+        self.file_path = f"./data/{self.name}.csv"
+        self.t_discount = t_discount
+        self.data = set()
 
+    def linear_equation(self, x, time):
+        self.k = random.uniform(-0.2, 0.2) + self.k
+        self.b = random.uniform(-1.8, 1.8) + self.b
+        return self.k * x + self.b - self.t_discount * time
 
-def func3(x):
-    return 6.15 * x - 2.33
-
-
-def disturbance(value, seed, positive=True):
-    if positive:
-        if value - seed < 0:
-            return 0
+    @classmethod
+    def disturbance(cls, value, positive=True):
+        dis_range = 10
+        ran = random.random()
+        dis_num = 2 * dis_range if ran > 0.8 else 3 * dis_range
+        value += random.uniform(-dis_num, dis_num)
+        if positive:
+            return value if value > 0 else 0
         else:
-            return value - seed
-    else:
-        return value - seed
+            return value
+
+    def generate_ev_data(self):
+        with open(self.file_path, "w") as f:
+            f.write(','.join(self.attribute) + "\n")
+            for i in np.arange(0, 100.0, 0.5):
+                time = round(self.disturbance(time_random(300)))
+                distance = round(self.disturbance(self.linear_equation(i, time)), 2)
+                self.data.add("%s,%s,%s\n" % (i, time, distance))
+
+            for line in self.data:
+                f.write(line)
 
 
-if __name__ == "__main__":
-    func_set = (func1, func2, func3)
-
-    with open("data/data.csv", "w") as f:
-        f.write("soc,time,distance\n")
-        data = set()
-        for x in np.arange(0, 100.0, 1):
-            index = random.randint(0, len(func_set) - 1)
-            ran = random.random()
-            if ran > 0.8:
-                time_seed = random.uniform(-10, 12)
-                distance_seed = random.uniform(-60, 60)
-            else:
-                time_seed = random.uniform(-30, 20)
-                distance_seed = random.uniform(-60, 60)
-
-            soc = round(x, 2)
-            time = round(disturbance(x, time_seed))
-            distance = round(disturbance(func_set[index](x), distance_seed) - 0.23 * time, 2)
-            data.add("%s,%s,%s\n" % (soc, time, distance if distance > 0 else 0))
-
-        for info in data:
-            f.write(info)
+def time_random(upper_bound):
+    return random.uniform(0, upper_bound)
---
+++ code/starter.py
@@ -0,0 +1,11 @@
+from random_data import EV
+from LinearRegression.MultivariateLinearReg import multivariate_linear_reg
+
+if __name__ == "__main__":
+    ev_1 = EV(name="ev_1", k=5, b=5.2, t_discount=0.46)
+    # ev_1.generate_ev_data()
+    multivariate_linear_reg(ev_1.file_path, ev_1.attribute, 1000)
+
+    # ev_2 = EV(name="ev_2", k=3, b=2.2, t_discount=0.26)
+    # ev_2.generate_ev_data()
+    # multivariate_linear_reg(ev_2.file_path, ev_2.attribute)
