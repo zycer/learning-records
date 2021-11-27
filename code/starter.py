d49960f090518724a8f84375f27a09c553af9197
Zycer <1171881160@qq.com>
2021-11-27 23:15:11 +0800

    feat: 新增非线性数据模拟

--- code/starter.py
+++ code/starter.py
@@ -2,10 +2,12 @@ from random_data import EV
 from LinearRegression.MultivariateLinearReg import multivariate_linear_reg
 
 if __name__ == "__main__":
-    ev_1 = EV(name="ev_1", k=5, b=5.2, t_discount=0.46)
-    # ev_1.generate_ev_data()
-    multivariate_linear_reg(ev_1.file_path, ev_1.attribute, 1000)
+    # 线性
+    # ev_1 = EV(name="ev_1", k=5, b=5.2, t_discount=0.46)
+    # ev_1.generate_ev_data(reg_type="linear")
+    # multivariate_linear_reg(ev_1.file_path, ev_1.attribute, 1000)
 
-    # ev_2 = EV(name="ev_2", k=3, b=2.2, t_discount=0.26)
-    # ev_2.generate_ev_data()
-    # multivariate_linear_reg(ev_2.file_path, ev_2.attribute)
+    # 非线性
+    ev_2 = EV(name="ev_2", k=4, b=3.2, t_discount=0.46)
+    ev_2.generate_ev_data(reg_type="cc")
+    multivariate_linear_reg(ev_2.file_path, ev_2.attribute, 1000)
