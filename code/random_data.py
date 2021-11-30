a8363b7765fd8ce0a7f1840b699a23fbfedc77e8
Zycer <1171881160@qq.com>
2021-11-30 19:27:06 +0800

    fix: 修改数据生成方式

--- code/random_data.py
+++ code/random_data.py
@@ -19,27 +19,41 @@ class EV:
         self.data = set()
 
     def linear_equation(self, x, time):
+        x = self.disturbance(x)
+        time = self.disturbance(time)
         self.k = random.uniform(-0.2, 0.2) + self.k
         self.b = random.uniform(-1.8, 1.8) + self.b
         return self.k * x + self.b - self.t_discount * time
 
+    def non_linear_equation(self, x, time):
+        x = self.disturbance(x)
+        time = self.disturbance(time)
+        self.k += x / (x * 40 + 1)
+        self.k = random.uniform(-0.4, 0.4) + self.k
+        self.b = random.uniform(-2.8, 2.8) + self.b
+
+        return self.k * x + self.b - self.t_discount * time
+
     @classmethod
-    def disturbance(cls, value, positive=True):
+    def disturbance(cls, value, positive=True, half=False):
         dis_range = 10
         ran = random.random()
-        dis_num = 2 * dis_range if ran > 0.8 else 3 * dis_range
+        dis_num = 1.5 * dis_range if ran > 0.8 else 2 * dis_range
         value += random.uniform(-dis_num, dis_num)
+        value = value / 2 if half else value
         if positive:
             return value if value > 0 else 0
         else:
             return value
 
-    def generate_ev_data(self):
+    def generate_ev_data(self, reg_type):
+        func = self.linear_equation if reg_type == "linear" else self.non_linear_equation
+        half = False if reg_type != "linear" else True
         with open(self.file_path, "w") as f:
             f.write(','.join(self.attribute) + "\n")
             for i in np.arange(0, 100.0, 0.5):
                 time = round(self.disturbance(time_random(300)))
-                distance = round(self.disturbance(self.linear_equation(i, time)), 2)
+                distance = round(self.disturbance(func(i, time), half=half), 2)
                 self.data.add("%s,%s,%s\n" % (i, time, distance))
 
             for line in self.data:
