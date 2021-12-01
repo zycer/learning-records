ae21e9998022991c2e158951e5c9549369288577
zycer <35062742+zycer@users.noreply.github.com>
2021-12-01 18:40:00 +0800

    Update starter.py

--- code/starter.py
+++ code/starter.py
@@ -1,6 +1,9 @@
-from random_data import EV
+import random
+
+from random_data2 import EV
 from LinearRegression.MultivariateLinearReg import multivariate_linear_reg
 from LinearRegression.multinomial_regression import multinomial_reg
+from LinearRegression.univariate_linear_reg import linear_reg
 
 if __name__ == "__main__":
     # 线性
@@ -9,6 +12,12 @@ if __name__ == "__main__":
     # multivariate_linear_reg(ev_1.file_path, ev_1.attribute, 1000)
 
     # 非线性
-    ev_2 = EV(name="ev_2", k=4, b=3.2, t_discount=0.46)
+    # ev_2 = EV(name="ev_2", k=4, b=3.2, t_discount=0.46)
     # ev_2.generate_ev_data(reg_type="non_linear")
-    multinomial_reg(ev_2.file_path, ev_2.attribute)
+    # multinomial_reg(ev_2.file_path, ev_2.attribute)
+
+    ev_3 = EV("ev_3", 1.024, 100, 20, 3000, 0, 60)
+    ev_3.generate_ev_data(100)
+    # linear_reg(ev_3.file_path, ("weight", "distance"))
+
+    multinomial_reg(ev_3.file_path, ("soc", "battery_health", "distance"))
