719a77339e56005091baae99a402f18bb6d5961b
zycer <35062742+zycer@users.noreply.github.com>
2021-12-02 11:00:06 +0800

    Update multivariate_multinomial_regression.py

--- code/LinearRegression/multivariate_multinomial_regression.py
+++ code/LinearRegression/multivariate_multinomial_regression.py
@@ -1,14 +1,14 @@
+import json
+
 from sklearn import preprocessing as sp
 from sklearn import pipeline as pl
 from sklearn import linear_model as lm
 from sklearn import metrics as sm
 import pandas as pd
-import numpy as np
-import plotly.graph_objs as go
-import plotly
+import matplotlib.pyplot as plt
 
 
-def multivariate_mul_regression(data_path, attr):
+def multivariate_mul_regression(data_path, effect_data_path, attr):
     data = pd.read_csv(data_path)
     train_data = data.sample(frac=0.8)
     test_data = data.drop(train_data.index)
@@ -35,4 +35,24 @@ def multivariate_mul_regression(data_path, attr):
     print(sm.mean_squared_error(y_test, z_predictions))
     print(sm.median_absolute_error(y_test, z_predictions))
     print(sm.r2_score(y_test, z_predictions))
+
+    with open(effect_data_path, "r") as f:
+        effect_data: dict = json.load(f)
+
+    for key, value in effect_data.items():
+        x_data, y_data = [], []
+        for item_key, item_value in value.items():
+            if item_key == "key":
+                input_param_name, output_param_name = item_value
+                plt.xlabel(input_param_name)
+                plt.ylabel(output_param_name)
+            else:
+                x_data = [i[0] for i in item_value]
+                y_data = [i[1] for i in item_value]
+
+        plt.scatter(x_data, y_data, label="Train data")
+
+        plt.title("Relationship between SOC and distance")
+        plt.legend()
+        plt.show()
 
