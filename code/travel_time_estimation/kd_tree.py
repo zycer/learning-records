5435b2899917b36560fa737ff6acb8c4cd991bfd
Zycer <1171881160@qq.com>
2022-01-10 07:50:27 +0800

    fix and feat

--- code/travel_time_estimation/kd_tree.py
+++ code/travel_time_estimation/kd_tree.py
@@ -4,43 +4,100 @@ from operator import itemgetter
 import itertools
 import numpy as np
 
-lines = [[[0, 5], [3, 0.8]], [[4.5, 1.2], [9, 4.5]], [[11, 5.6], [4.8, 10.5]]]
-points = [[0, 0], [7, 5]]
 
-line_ix = tuple(itertools.chain.from_iterable(
-    [itertools.repeat(i, x) for i, x in enumerate(list(map(len, lines)))]
-))
-line_list = np.concatenate(lines)
-tree = spatial.cKDTree(line_list)
+class KNN:
+    def __init__(self, trajectory, segments, k=2):
+        self.trajectory = trajectory
+        self.segments = segments
+        self.k = k
+        self.segment_list = np.concatenate(segments)
+        self.tree = spatial.cKDTree(self.segment_list)
+        self.segments_ix = tuple(itertools.chain.from_iterable(
+            [itertools.repeat(i, road) for i, road in enumerate(list(map(len, self.segments)))]
+        ))
 
-dist, idx = tree.query(points, k=1)
+    def matched_segments(self):
+        matched_segment_idx = []
+        distance, roads_idx = self.tree.query(self.trajectory, k=self.k)
+        for road_id in roads_idx:
+            matched_segment_idx.append(itemgetter(*road_id)(self.segments_ix))
+        print("结果：")
+        print(matched_segment_idx, distance)
 
-idx = itemgetter(*idx)(line_ix)
+    def plot_result(self):
+        x_list = []
+        y_list = []
+        for line in self.segments:
+            for num, value in enumerate(zip(line[0], line[1])):
+                if num % 2 == 0:
+                    x_list.append(value)
+                else:
+                    y_list.append(value)
 
-print("树结构~~~~~~~~~~")
-print(tree.data)
-print()
+        point_x = []
+        point_y = []
+        for point in self.trajectory:
+            point_x.append(point[0])
+            point_y.append(point[1])
 
-print("结果：")
-print(idx, dist)
-x_list = []
-y_list = []
-for line in lines:
-    for num, value in enumerate(zip(line[0], line[1])):
-        if num % 2 == 0:
-            x_list.append(value)
-        else:
-            y_list.append(value)
+        for i in range(len(x_list)):
+            plt.plot(x_list[i], y_list[i], label=i)
 
-point_x = []
-point_y = []
-for point in points:
-    point_x.append(point[0])
-    point_y.append(point[1])
+        plt.scatter(point_x, point_y)
+        plt.legend(loc=0, ncol=2)
+        plt.show()
 
 
-for i in range(len(x_list)):
-    plt.plot(x_list[i], y_list[i])
+if __name__ == "__main__":
+    points = [[7, 5], [10, 8]]
+    lines = [[[0, 5], [3, 0.8]], [[4.5, 1.2], [9, 4.5]], [[11, 5.6], [4.8, 10.5]], [[18, 9], [20, 15]],
+             [[12, 3.1], [17, 5.5]], [[13, 7.8], [15.5, 16]]]
+    knn = KNN(points, lines)
+    knn.matched_segments()
+    knn.plot_result()
 
-plt.scatter(point_x, point_y)
-plt.show()
+#
+# def knn(gps_trajectory, segments, k=2):
+#     """
+#     param: gps_point: 浮动车gps轨迹点列表
+#     eg. [[7, 5], [10, 8]]
+#     param: segments: 要匹配的路段
+#     eg. [[[0, 5], [3, 0.8]], [[4.5, 1.2], [9, 4.5]], [[11, 5.6], [4.8, 10.5]], [[18, 9], [20, 15]]]
+#     获取距离gps点最近的k个路段
+#     """
+#     # matched_segment_idx = []
+#     # segments_ix = tuple(itertools.chain.from_iterable(
+#     #     [itertools.repeat(i, road) for i, road in enumerate(list(map(len, segments)))]
+#     # ))
+#     # segment_list = np.concatenate(segments)
+#     # ckd_tree = spatial.cKDTree(segment_list)
+#     # dist, idx = ckd_tree.query(gps_trajectory, k=k)
+#     # for road_id in idx:
+#     #     matched_segment_idx.append(itemgetter(*road_id)(segments_ix))
+#
+#     print("结果：")
+#     print(matched_segment_idx, dist)
+#     x_list = []
+#     y_list = []
+#     for line in segments:
+#         for num, value in enumerate(zip(line[0], line[1])):
+#             if num % 2 == 0:
+#                 x_list.append(value)
+#             else:
+#                 y_list.append(value)
+#
+#     point_x = []
+#     point_y = []
+#     for point in gps_trajectory:
+#         point_x.append(point[0])
+#         point_y.append(point[1])
+#
+#     for i in range(len(x_list)):
+#         plt.plot(x_list[i], y_list[i], label=i)
+#
+#     plt.scatter(point_x, point_y)
+#     plt.legend(loc=0, ncol=2)
+#     plt.show()
+
+
+
--- code/travel_time_estimation/main.py
+++ code/travel_time_estimation/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from get_data import GPSData
+from kd_tree import KNN
 
 
 class Vertex:
@@ -52,61 +53,59 @@ def create_graph(matrix):
     return rng
 
 
+class AIVMM:
+    def __init__(self):
+        pass
+
+    # 位置和道路分析
+    def gps_observation_probability(self, mu, sigma, x_ij):
+        """
+        param: x_ij: 候选点i到j的欧氏距离
+        GPS点的观测概率
+        """
+        return np.exp(-(x_ij - mu) ** 2 / 2 * (sigma ** 2))
+
+    def excess_probability(self, distance, omega):
+        """
+        param: distance: 采样点i-1到采样点i的欧氏距离
+        param: omega: 从候选点i-1到i的最短路径长度
+        过滤概率函数
+        """
+        return distance / omega
+
+    def spatial_analysis(self, gop, ep):
+        """
+        param: gop: GPS点的观测概率
+        param: ep: 过度概率
+        空间分析函数
+        """
+        return gop * ep
+
+    def time_analysis(self, hat_v, overline_v):
+        """
+        param: hat_v: 候选点c_i-1到c_i的最短路径的加权速度限制
+        param: hat_v: 沿着候选点c_i-1到c_i之间最短路径行驶的车辆额平均速度
+        时间分析函数
+        """
+        return hat_v / (abs(hat_v - overline_v) + hat_v)
+
+    def road_level_factor(self, vs, vd):
+        """
+        param: vs: 候选点c_i-1所在道路限速
+        param: vd: 候选点c_i所在道路限速
+        道路水平系数RLF
+        """
+        return vs / ((vd - vs) + vs)
+
+    def path_weight(self, fs, ft, rlf):
+        """
+        param: fs: 空间分析函数
+        param: ft: 时间分析函数
+        param: rlf: 道路水平因子
+        """
+        return fs * ft * rlf
 
 
-
-# 位置和道路分析
-def gps_observation_probability(mu, sigma, x_ij):
-    """
-    param: x_ij: 候选点i到j的欧氏距离
-    GPS点的观测概率
-    """
-    return np.exp(-(x_ij - mu) ** 2 / 2 * (sigma ** 2))
-
-
-def excess_probability(distance, omega):
-    """
-    param: distance: 采样点i-1到采样点i的欧氏距离
-    param: omega: 从候选点i-1到i的最短路径长度
-    过滤概率函数
-    """
-    return distance / omega
-
-
-def spatial_analysis(gop, ep):
-    """
-    param: gop: GPS点的观测概率
-    param: ep: 过度概率
-    空间分析函数
-    """
-    return gop * ep
-
-
-def time_analysis(hat_v, overline_v):
-    """
-    param: hat_v: 候选点c_i-1到c_i的最短路径的加权速度限制
-    param: hat_v: 沿着候选点c_i-1到c_i之间最短路径行驶的车辆额平均速度
-    时间分析函数
-    """
-    return hat_v / (abs(hat_v - overline_v) + hat_v)
-
-
-def road_level_factor(vs, vd):
-    """
-    param: vs: 候选点c_i-1所在道路限速
-    param: vd: 候选点c_i所在道路限速
-    道路水平系数RLF
-    """
-    return vs / ((vd - vs) + vs)
-
-
-def path_weight(fs, ft, rlf):
-    """
-    param: fs: 空间分析函数
-    param: ft: 时间分析函数
-    param: rlf: 道路水平因子
-    """
-    return fs * ft * rlf
 
 
 # 相互影响分析
