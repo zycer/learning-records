4129034b2c8e2658f354270a3b772f497de6868c
zycer <35062742+zycer@users.noreply.github.com>
2022-05-27 17:08:31 +0800

    Update speed_distribution.py

--- code/travel_time_estimation/speed_distribution.py
+++ code/travel_time_estimation/speed_distribution.py
@@ -4,8 +4,8 @@ from pathlib import Path
 
 import networkx as nx
 import pandas as pd
-from constants import ROAD_DATA_PATH, INTERSEC_DATA_PATH, ROAD_ATTR, INTERSEC_ATTR
-from utils import get_road_data, get_intersection_data
+from constants import ROAD_DATA_PATH, INTERSEC_DATA_PATH, ROAD_ATTR, INTERSEC_ATTR, GRAPH_DATA
+from utils import get_road_data, get_intersection_data, get_graph_data
 from db_manager import DBManager
 
 
@@ -15,23 +15,39 @@ class BayesianEstimate:
         self.time_frame = 6000
         self.db_handler = DBManager()
         self.get_basic_traffic_graph()
+        self.roads_dict = {}
+        self.intersection_dict = {}
 
     def get_basic_traffic_graph(self):
         road_data = [get_road_data(road_file) for road_file in
                      Path(ROAD_DATA_PATH).iterdir()]  # save road data iterator
         intersection_data = [get_intersection_data(intersection_file) for intersection_file in
                              Path(INTERSEC_DATA_PATH).iterdir()]  # save intersection data iterator
+        graph_data = [get_graph_data(graph_file) for graph_file in
+                      Path(GRAPH_DATA).iterdir()]
+
         traffic_graph = nx.DiGraph()
 
         for road_iter in road_data:
             for road_one_info in road_iter:
                 road_attr_dict = dict(zip(ROAD_ATTR, road_one_info))
-                traffic_graph.add_edge(road_attr_dict["from_node_id"], road_attr_dict["to_node_id"], **road_attr_dict)
+                self.roads_dict[road_attr_dict["link_id"]] = road_attr_dict
+                # traffic_graph.add_edge(road_attr_dict["from_node_id"], road_attr_dict["to_node_id"], **road_attr_dict)
 
         for intersec_iter in intersection_data:
             for intersec_one_info in intersec_iter:
                 intersec_attr_dict = dict(zip(INTERSEC_ATTR, intersec_one_info))
-                traffic_graph.add_node(intersec_attr_dict["node_id"], **intersec_attr_dict)
+                self.intersection_dict[intersec_attr_dict["node_id"]] = intersec_attr_dict
+                # traffic_graph.add_node(intersec_attr_dict["node_id"], **intersec_attr_dict)
+
+        for road_one in self.roads_dict:
+            link_id = road_one["link_id"]
+            del road_one["link_id"]
+            traffic_graph.add_node(link_id, road_attr=tuple([value for value in road_one]))
+
+        for graph_iter in graph_data:
+            for edge in graph_iter:
+                traffic_graph.add_edge(edge[0], edge[1], edge_id=edge[2])
 
         self.basic_graph = traffic_graph
 
@@ -65,15 +81,24 @@ class BayesianEstimate:
                 finish_one_flag = False
 
             for road_one in history_data:
-                fro = int(road_one[3])
-                to = int(road_one[4])
+                road_id = int(road_one[0])
                 history_speed_dict = json.loads(road_one[1])
-
                 for timestamp in history_speed_dict.keys():
                     if start_timestamp <= int(timestamp) <= end_timestamp:
-                        copy_traffic_graph.edges[fro, to]["history_speed"] = history_speed_dict[timestamp]
+                        copy_traffic_graph.nodes[road_id]["history_speed"] = history_speed_dict[timestamp]
                         matched_roads_num += 1
                         break
+
+            # for road_one in history_data:
+            #     fro = int(road_one[3])
+            #     to = int(road_one[4])
+            #     history_speed_dict = json.loads(road_one[1])
+            #
+            #     for timestamp in history_speed_dict.keys():
+            #         if start_timestamp <= int(timestamp) <= end_timestamp:
+            #             copy_traffic_graph.edges[fro, to]["history_speed"] = history_speed_dict[timestamp]
+            #             matched_roads_num += 1
+            #             break
 
             # 匹配完成标志，如果考虑计算复杂度，可以设置差值
             # if matched_roads_num >= len(copy_traffic_graph.edges):
@@ -85,4 +110,6 @@ class BayesianEstimate:
         print(multi_traffic_graph)
 
 
-BayesianEstimate().generate_multi_traffic_graph()
+# BayesianEstimate().generate_multi_traffic_graph()
+
+
