84e007fba0d78a827abdae1fe1cb12cb5e754b8e
Zycer <1171881160@qq.com>
2022-06-02 16:04:36 +0800

    fix: 优化匹配速度

--- code/time_estimate/utils/db_manager.py
+++ code/time_estimate/utils/db_manager.py
@@ -128,3 +128,19 @@ class DBManager:
     def close(self):
         self.cur.close()
         self.conn.close()
+
+        class NpEncoder(json.JSONEncoder):
+            def default(self, obj):
+                if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+                                    np.int16, np.int32, np.int64, np.uint8,
+                                    np.uint16, np.uint32, np.uint64)):
+                    return int(obj)
+                elif isinstance(obj, (np.float_, np.float16, np.float32,
+                                      np.float64)):
+                    return float(obj)
+                elif isinstance(obj, (np.ndarray,)):  # add this line
+                    return obj.tolist()  # add this line
+                return json.JSONEncoder.default(self, obj)
+
+
+
