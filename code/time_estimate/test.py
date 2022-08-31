3735b83d210f9eda7229ab6e9ad34e2f1892a117
Zycer <1171881160@qq.com>
2022-08-31 23:41:09 +0800

    feat: 更新论文0831

--- code/time_estimate/test.py
+++ code/time_estimate/test.py
@@ -1,15 +1,18 @@
-    def new_find_lops(self, omega_i, phi_i, n, candi_count, ii):
-        f_values = np.zeros((n, candi_count[0]))
-        for i in range(n):
-            if i == 0:
-                for t in range(n):
-                    temp_value = omega_i[ii, ii] * self.candidate_graph_obj.candidate_graph.nodes[f"{0}&{t}"][
-                        "observation_probability"]
-                    f_values[0, t] = temp_value
-            else:
-                for s in range(candi_count[i]):
-                    f_values[i, s] = np.max([f_values[i-1][t]+phi_i[i-1][t,s] for t in range(candi_count[i])])
+import pandas as pd
+import os
 
-        print(f_values)
 
-        return f_values
+def statistic_rows_num():
+    total = 0
+    for file_name in os.listdir("data/gps_trajectory"):
+        df = pd.read_csv(os.path.join("data/gps_trajectory", file_name))
+        print(df.shape)
+        total += df.shape[0]
+
+    print(total)
+
+
+def statistic_points_num():
+    for file_name in os.listdir("data/gps_trajectory"):
+        df = pd.read_csv(os.path.join("data/gps_trajectory", file_name))
+
