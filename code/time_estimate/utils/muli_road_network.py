3a866799d64549995e1f1a4e8f9684f1184348ee
zycer <35062742+zycer@users.noreply.github.com>
2022-10-11 17:18:48 +0800

    Create muli_road_network.py

---
+++ code/time_estimate/utils/muli_road_network.py
@@ -0,0 +1,45 @@
+import json
+from collections import OrderedDict
+
+from road_network import BaseRoadNetwork
+from db_manager import DBManager
+
+
+class MultiRoadNetwork(BaseRoadNetwork):
+    def __init__(self, usage):
+        self.db_manager = DBManager()
+        self.multi_road_network = []
+        self.group_road_data = self.time_group_data()
+        super().__init__(usage)
+
+    def time_group_data(self):
+        sql = "SELECT * FROM history_road_data limit 0, 10"
+        query_data = self.db_manager.exec_sql(sql)
+        road_data_dict = {}
+        for one_data in query_data:
+            in_one_road = OrderedDict()
+            one_speed_data = {}
+            for _dict in one_data[1].split(";"):
+                one_speed_data.update(json.loads(_dict))
+
+            for key in sorted(one_speed_data.keys()):
+                in_one_road[key] = one_speed_data[key]
+            road_data_dict[one_data[0]] = in_one_road
+
+        return road_data_dict
+
+    def generate_multi_road_network(self):
+        self.init_graph()
+        for road_id, one_road_data in self.group_road_data.items():
+            if one_road_data:
+                timestamp = list(one_road_data.keys())[0]
+                self.road_graph.nodes[int(road_id)]["road_attr"][-1] = one_road_data[timestamp]
+
+        print(self.road_graph.nodes[11212])
+
+
+if __name__ == '__main__':
+    multi_network = MultiRoadNetwork("gcn")
+    multi_network.time_group_data()
+
+    multi_network.generate_multi_road_network()
