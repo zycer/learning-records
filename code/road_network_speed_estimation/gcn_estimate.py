e1a432c5378a157f38ca6eaf1282ce7c807cea55
Zycer <1171881160@qq.com>
2022-10-18 14:37:08 +0800

    feat: GCN

--- code/road_network_speed_estimation/gcn_estimate.py
+++ code/road_network_speed_estimation/gcn_estimate.py
@@ -16,7 +16,7 @@ def z_score(raw_data):
 class RoadNetworkGraphData(InMemoryDataset):
     def __init__(self, root="data/run_data", transform=None, pre_transform=None):
         self.root = root
-        self.multi_graph_path = "data/multi_graph"
+        self.multi_graph_path = "data/multi_graphh"
         self.data, self.slices = torch.load(self.processed_paths[0])
         super(RoadNetworkGraphData, self).__init__(root, transform, pre_transform)
 
@@ -57,46 +57,48 @@ class RoadNetworkGraphData(InMemoryDataset):
         graph_data, slices = self.collate(data_list)
         torch.save((graph_data, slices), self.processed_paths[0])
 
-
-class GCN(torch.nn.Module):
-    def __init__(self, hidden_channels):
-        super().__init__()
-        torch.manual_seed(112233)
-        self.conv1 = GCNConv(datasets.num_features, hidden_channels)
-        self.conv2 = GCNConv(hidden_channels, datasets.num_classes)
-
-    def forward(self, x, edge_index):
-        x = self.conv1(x, edge_index)
-        x = x.relu()
-        x = F.dropout(x, p=0.5, trainning=self.training)
-        x = self.conv2(x, edge_index)
-
-        return x
-
-
-def train():
-    model.train()
-    optimizer.zero_grad()
-    out = model(data.x, data.edge_index)
-    loss = criterion(out[data.train_mask], data.y[data.train_mask])
-    loss.backward()
-    optimizer.step()
-
-    return loss
-
-
-def test():
-    model.eval()
-    out = model(data.x, data.edge_index)
-    pred = out.argmax(dim=1)
-    test_correct = pred[data.test_mask] == data.y[data.train_mask]
-    test_acc = int(test_correct.sum()) / int(data.test_mask.sum())
-
-    return test_acc
+    #
+    # class GCN(torch.nn.Module):
+    #     def __init__(self, hidden_channels):
+    #         super().__init__()
+    #         torch.manual_seed(112233)
+    #         self.conv1 = GCNConv(datasets.num_features, hidden_channels)
+    #         self.conv2 = GCNConv(hidden_channels, datasets.num_classes)
+    #
+    #     def forward(self, x, edge_index):
+    #         x = self.conv1(x, edge_index)
+    #         x = x.relu()
+    #         x = F.dropout(x, p=0.5, trainning=self.training)
+    #         x = self.conv2(x, edge_index)
+    #
+    #         return x
+    #
+    #
+    # def train():
+    #     model.train()
+    #     optimizer.zero_grad()
+    #     out = model(data.x, data.edge_index)
+    #     loss = criterion(out[data.train_mask], data.y[data.train_mask])
+    #     loss.backward()
+    #     optimizer.step()
+    #
+    #     return loss
+    #
+    #
+    # def test():
+    #     model.eval()
+    #     out = model(data.x, data.edge_index)
+    #     pred = out.argmax(dim=1)
+    #     test_correct = pred[data.test_mask] == data.y[data.train_mask]
+    #     test_acc = int(test_correct.sum()) / int(data.test_mask.sum())
+    #
+    #     return test_acc
 
 
 if __name__ == '__main__':
-    model = GCN(hidden_channels=16)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
-    criterion = torch.nn.CrossEntropyLoss()
+    RoadNetworkGraphData()
+    # model = GCN(hidden_channels=16)
+    # optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+    # criterion = torch.nn.CrossEntropyLoss()
     # data = []
+
