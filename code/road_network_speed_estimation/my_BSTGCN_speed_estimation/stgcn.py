963e31b2c0fc25aca2466785996d5731d38d1f31
zycer <35062742+zycer@users.noreply.github.com>
2023-03-23 21:32:05 +0800

    Update stgcn.py

--- code/road_network_speed_estimation/my_BSTGCN_speed_estimation/stgcn.py
+++ code/road_network_speed_estimation/my_BSTGCN_speed_estimation/stgcn.py
@@ -3,48 +3,62 @@ import pickle
 
 import numpy as np
 import torch
+from torch_geometric.data import Data
 
 from map_matching.utils.road_network import BaseRoadNetwork
-from torch_geometric_temporal import StaticGraphTemporalSignal
-
-from road_network_speed_estimation.utils import z_score
 
 
-class STRoadGraph(StaticGraphTemporalSignal):
+class STRoadGraph:
     def __init__(self, st_graph_data):
         graph_obj = BaseRoadNetwork("gcn")
         graph_obj.init_graph()
         graph_obj.road_graph.to_undirected()
 
-        graph_attr = []  # [timestamp1, timestamp2,...]
-        road_attr = []  # [(限速,车道数,长度),...]
-        edge_attr = []  # [两个道路之间的行驶时间,...]
+        graph_attrs = []
+        road_attrs = []
+        edge_attrs = []
+        targets = []
 
         for _timestamp, _graph_data in st_graph_data.items():
-            graph_attr.append(_timestamp)
+            graph_attrs.append(_timestamp)  # [timestamp1, timestamp2,...]
+            road_attr = []  # [(限速,车道数,长度),...]
+            edge_attr = []  # [两个道路之间的行驶时间,...]
+            target = [] # [road_travel_time,...]
             free_speed_roads = [graph_obj.road_graph.nodes[_road_id]["free_speed"] for _road_id in graph_obj.road_graph.nodes]
             lanes_roads = [graph_obj.road_graph.nodes[_road_id]["lanes"] for _road_id in graph_obj.road_graph.nodes]
             length_roads = [graph_obj.road_graph.nodes[_road_id]["length"] for _road_id in graph_obj.road_graph.nodes]
-
+            target = [graph_obj.road_graph.nodes[_road_id]["length"] / _graph_data[_road_id] for _road_id in graph_obj.road_graph.nodes]
             for one_road_attr in zip(free_speed_roads, lanes_roads, length_roads):
                 road_attr.append(one_road_attr)
 
             for from_road_id, to_road_id in graph_obj.road_graph.edges:
-                pass
+                edge_attr.append(graph_obj.road_graph.nodes[from_road_id]["length"] / _graph_data[from_road_id])
 
-
+            road_attrs.append(road_attr)
+            edge_attrs.append(edge_attr)
+            targets.append(target)
 
         # 道路特征张量化
-        node_features_transformed = torch.FloatTensor(z_score(node_features))
-        labels_transformed = torch.FloatTensor(z_score(np.array(labels).reshape(-1, 1)))
-
+        # road_attrs_transformed = torch.FloatTensor(z_score(road_attrs))
+        # edge_attr_transformed = torch.FloatTensor(z_score(edge_attrs))
         source_nodes = list(map(lambda x: int(x[0]), graph_obj.road_graph.edges))
         target_nodes = list(map(lambda x: int(x[1]), graph_obj.road_graph.edges))
-        edge_index = torch.tensor([source_nodes, target_nodes], dtype=torch.long)
 
-        for _timestamp, _graph_data in st_graph_data:
-            pass
-        super(STRoadGraph, self).__init__()
+        self.edge_index = torch.tensor([source_nodes, target_nodes], dtype=torch.long)
+        self.edge_attrs = torch.tensor(edge_attrs, dtype=torch.float64)
+        self.road_attrs = torch.tensor(road_attrs, dtype=torch.float64)
+        self.targets = torch.tensor(targets, dtype=torch.float64)
+        self.graph_attrs = torch.tensor(graph_attrs, dtype=torch.long)
+
+    def __getitem__(self, index):
+        x = self.road_attrs[index]
+        y = self.targets[index]
+        edge_index = self.edge_index
+        edge_attr = self.edge_attrs[index]
+        return Data(x=x, y=y, edge_index=edge_index, edge_attr=edge_attr, graph_attr=self.graph_attrs[index])
+
+    def __len__(self):
+        return len(self.graph_attrs)
 
 
 def get_st_road_graph(_index):
@@ -56,10 +70,6 @@ def get_st_road_graph(_index):
 if __name__ == '__main__':
     data_path = "data"
     st_graph_files = os.listdir(data_path)
-    #
-    # with open(os.path.join(data_path, st_graph_files[0]), "rb") as f:
-    #     file = pickle.load(f)
-    #
-    #     for timestamp, graph_data in file.items():
-    #         print(timestamp, len(graph_data))
 
+    patch_1 = get_st_road_graph(0)
+    print(patch_1[0])
