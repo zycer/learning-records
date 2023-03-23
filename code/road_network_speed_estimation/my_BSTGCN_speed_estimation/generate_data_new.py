5edd42e09f5487dc20516481ee87f28542a95759
zycer <35062742+zycer@users.noreply.github.com>
2023-03-23 21:29:16 +0800

    Update generate_data_new.py

--- code/road_network_speed_estimation/my_BSTGCN_speed_estimation/generate_data_new.py
+++ code/road_network_speed_estimation/my_BSTGCN_speed_estimation/generate_data_new.py
@@ -1,78 +1,126 @@
-import os
+import copy
 import pickle
+import json
+import sys
+import math
+import random
+import threading
 
-import numpy as np
-import torch
-from torch_geometric.data import Data
+import tqdm
+from collections import OrderedDict
+from datetime import datetime
 
 from map_matching.utils.road_network import BaseRoadNetwork
-from torch_geometric_temporal import StaticGraphTemporalSignal
-
-from road_network_speed_estimation.utils import z_score
-
-
-class STRoadGraph:
-    def __init__(self, st_graph_data):
-        graph_obj = BaseRoadNetwork("gcn")
-        graph_obj.init_graph()
-        graph_obj.road_graph.to_undirected()
-
-        graph_attrs = []
-        road_attrs = []
-        edge_attrs = []
-        targets = []
-
-        for _timestamp, _graph_data in st_graph_data.items():
-            graph_attrs.append(_timestamp)  # [timestamp1, timestamp2,...]
-            road_attr = []  # [(限速,车道数,长度),...]
-            edge_attr = []  # [两个道路之间的行驶时间,...]
-            target = [] # [road_travel_time,...]
-            free_speed_roads = [graph_obj.road_graph.nodes[_road_id]["free_speed"] for _road_id in graph_obj.road_graph.nodes]
-            lanes_roads = [graph_obj.road_graph.nodes[_road_id]["lanes"] for _road_id in graph_obj.road_graph.nodes]
-            length_roads = [graph_obj.road_graph.nodes[_road_id]["length"] for _road_id in graph_obj.road_graph.nodes]
-            target = [graph_obj.road_graph.nodes[_road_id]["length"] / _graph_data[_road_id] for _road_id in graph_obj.road_graph.nodes]
-            for one_road_attr in zip(free_speed_roads, lanes_roads, length_roads):
-                road_attr.append(one_road_attr)
-
-            for from_road_id, to_road_id in graph_obj.road_graph.edges:
-                edge_attr.append(graph_obj.road_graph.nodes[from_road_id]["length"] / _graph_data[from_road_id])
-
-            road_attrs.append(road_attr)
-            edge_attrs.append(edge_attr)
-            targets.append(target)
-
-        # 道路特征张量化
-        # road_attrs_transformed = torch.FloatTensor(z_score(road_attrs))
-        # edge_attr_transformed = torch.FloatTensor(z_score(edge_attrs))
-        source_nodes = list(map(lambda x: int(x[0]), graph_obj.road_graph.edges))
-        target_nodes = list(map(lambda x: int(x[1]), graph_obj.road_graph.edges))
-
-        self.edge_index = torch.tensor([source_nodes, target_nodes], dtype=torch.long)
-        self.edge_attrs = torch.tensor(edge_attrs, dtype=torch.float64)
-        self.road_attrs = torch.tensor(road_attrs, dtype=torch.float64)
-        self.targets = torch.tensor(targets, dtype=torch.float64)
-        self.graph_attrs = torch.tensor(graph_attrs, dtype=torch.long)
-
-    def __getitem__(self, index):
-        x = self.road_attrs[index]
-        y = self.targets[index]
-        edge_index = self.edge_index
-        edge_attr = self.edge_attrs[index]
-        return Data(x=x, y=y, edge_index=edge_index, edge_attr=edge_attr, graph_attr=self.graph_attrs[index])
-
-    def __len__(self):
-        return len(self.graph_attrs)
+from map_matching.utils.db_manager import DBManager
+import networkx as nx
 
 
-def get_st_road_graph(_index):
-    with open(os.path.join(data_path, st_graph_files[_index]), "rb") as f:
-        st_graph_data = pickle.load(f)
-    return STRoadGraph(st_graph_data)
+class MultiRoadNetwork(BaseRoadNetwork):
+    def __init__(self, usage):
+        self.db_manager = DBManager()
+        self.min_timestamp = sys.maxsize
+        self.time_step = 1800
+        self.all_road_data = self.get_all_road_data()
+        super().__init__(usage)
+
+    def get_all_road_data(self):
+        print("get_all_road_data.....")
+        sql = "SELECT * FROM history_road_data"
+        query_data = self.db_manager.exec_sql(sql)
+        road_data_dict = {}
+        for one_data in tqdm.tqdm(query_data):
+            in_one_road = OrderedDict()
+            one_speed_data = {}
+            for _dict in one_data[1].split(";"):
+                one_speed_data.update(json.loads(_dict))
+
+            for key in sorted([int(key) for key in one_speed_data.keys()]):
+                in_one_road[key] = one_speed_data[str(key)]
+                if key < self.min_timestamp:
+                    self.min_timestamp = key
+            road_data_dict[int(one_data[0])] = in_one_road
+
+        return road_data_dict
+
+    def generate_st_road_graph(self):
+        print("\ngenerate_st_road_graph...")
+        time_road_data = OrderedDict()
+        for road_id, one_values in tqdm.tqdm(self.all_road_data.items()):
+            for timestamp, speed in one_values.items():
+                time_step_num = math.ceil((timestamp - self.min_timestamp) / self.time_step)
+                time_window = self.min_timestamp + time_step_num * self.time_step
+                if time_window not in time_road_data.keys():
+                    time_road_data[time_window] = {}
+                if road_id not in time_road_data[time_window].keys():
+                    time_road_data[time_window][road_id] = []
+                time_road_data[time_window][road_id].append(speed)
+
+        return time_road_data
+
+
+def save_graph_data(_data):
+    __index, __timestamp_list, __data = _data
+    dump_data = {}
+    for temp_data in zip(__timestamp_list, __data):
+        dump_data[temp_data[0]] = temp_data[1]
+    with open(f"{data_path}/{__index}.pickle", "wb") as f:
+        pickle.dump(dump_data, f)
 
 
 if __name__ == '__main__':
-    data_path = "data"
-    st_graph_files = os.listdir(data_path)
+    threads = []
+    multi_network = MultiRoadNetwork("gcn")
+    multi_network.init_graph()
+    time_road_data = multi_network.generate_st_road_graph()
+    data_path = "../road_network_speed_estimation/my_BSTGCN_speed_estimation/data"
 
-    patch_1 = get_st_road_graph(0)
-    print(patch_1[0])
+    print("\nComplete data...")
+    timestamp_list = []
+    num = 0
+    for index, __values in tqdm.tqdm(enumerate(time_road_data.items()), total=len(time_road_data)):
+        timestamp, values = __values
+        for road_id, speeds in values.items():
+            average_speed = sum(speeds) / len(speeds)
+            time_road_data[timestamp][road_id] = average_speed
+
+        no_data_road_ids = list(set(multi_network.road_graph.nodes).difference(set(values.keys())))
+        for _road_id in no_data_road_ids:
+            if _road_id in multi_network.all_road_data.keys():
+                if multi_network.all_road_data[_road_id]:
+                    flag = False
+                    for _timestamp, _speed in multi_network.all_road_data[_road_id].items():
+                        dt1 = datetime.fromtimestamp(timestamp)
+                        dt2 = datetime.fromtimestamp(_timestamp)
+                        minutes1 = dt1.hour * 60 + dt1.minute
+                        minutes2 = dt2.hour * 60 + dt2.minute
+                        if abs(minutes2 - minutes1) <= multi_network.time_step * 2:
+                            time_road_data[timestamp][_road_id] = _speed + random.uniform(-5, 5)
+                            flag = True
+                            break
+                    if not flag:
+                        sum(multi_network.all_road_data[_road_id].values()) / len(
+                            multi_network.all_road_data[_road_id].values())
+                else:
+                    time_road_data[timestamp][_road_id] = multi_network.road_graph.nodes[_road_id][
+                                                              "free_speed"] + random.uniform(-5, 5)
+            else:
+                time_road_data[timestamp][_road_id] = multi_network.road_graph.nodes[_road_id][
+                                                          "free_speed"] + random.uniform(-5, 5)
+        timestamp_list.append(timestamp)
+        if len(timestamp_list) == 100:
+            t = threading.Thread(target=save_graph_data,
+                                 args=((num, timestamp_list, [time_road_data[temp] for temp in timestamp_list]),))
+            threads.append(t)
+            t.start()
+            timestamp_list.clear()
+            num += 1
+
+    if timestamp_list:
+        t = threading.Thread(target=save_graph_data,
+                             args=((num, timestamp_list, [time_road_data[temp] for temp in timestamp_list]),))
+        threads.append(t)
+        t.start()
+
+    print("\nWait for the child process to end...")
+    for t in tqdm.tqdm(threads):
+        t.join()
